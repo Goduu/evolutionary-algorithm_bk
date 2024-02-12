3f7552e35a2168800b8022194350528f574f375e
Goduu <idcangussu@hotmail.com>
2024-02-12 20:52:57 +0100

    Update environment and API URLs

--- next.config.js
+++ next.config.js
@@ -7,21 +7,21 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/:path*",
+            : "https://evolutionary-algorithm.vercel.app/api/:path*",
       },
       {
         source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/docs"
-            : "/api/docs",
+            : "https://evolutionary-algorithm.vercel.app/api/docs",
       },
       {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/openapi.json"
-            : "/api/openapi.json",
+            : "https://evolutionary-algorithm.vercel.app/api/openapi.json",
       },
     ];
   },
