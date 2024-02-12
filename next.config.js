6478b279056e4264d1b8e9e11289316d46778b75
Goduu <idcangussu@hotmail.com>
2024-02-12 20:59:49 +0100

    Update API endpoints in next.config.js

--- next.config.js
+++ next.config.js
@@ -7,21 +7,21 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/:path*"
-            : "https://evolutionary-algorithm.vercel.app/api/:path*",
+            : "/api/:path*",
       },
       {
         source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/docs"
-            : "https://evolutionary-algorithm.vercel.app/api/docs",
+            : "/api/docs",
       },
       {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/openapi.json"
-            : "https://evolutionary-algorithm.vercel.app/api/openapi.json",
+            : "/api/openapi.json",
       },
     ];
   },
