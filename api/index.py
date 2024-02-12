d85b8d54b9f74c4095cc89a30e6b4910094f1d53
Goduu <idcangussu@hotmail.com>
2024-02-12 18:53:04 +0100

    Update CORS settings in index.py

--- api/index.py
+++ api/index.py
@@ -12,8 +12,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://evolutionary-algorithm.vercel.app/", "http://localhost:3000/"],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["https://evolutionary-algorithm.vercel.app/", "http://localhost:3000/"],
+    allow_headers=["https://evolutionary-algorithm.vercel.app/", "http://localhost:3000/"],
 )
 
 class ResponseMessage(BaseModel):
